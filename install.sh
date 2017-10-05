ab095b13ce1f9cd0e1c73c67b3501325370605ad
AadityaNair <aadityanair6494@gmail.com>
2017-10-06 01:36:40 +0530

    Install script is finally ready

--- install.sh
+++ install.sh
@@ -1,34 +1,77 @@
 #!/usr/bin/zsh
 
+set -exo pipefail
+
+# TODO: Go back to old pwd
+# TODO: AutoInstall Plugins
 cd $HOME
 
+#Exit if repo already exists.
+if [ ! -d .dotfiles ]; then
+    echo "The repo seems already setup. Exiting"
+    exit 1
+fi
+
 #Download the repository
-git clone --depth 1 https://github.com/AadityaNair/dotfiles.git .dotfiles
+#git clone --depth 1 https://github.com/AadityaNair/dotfiles.git .dotfiles
 
 # 1. Install ZSH setup
-if [ -f ~/.zshrc ]; then
-    echo ".zshrc exists. taking backup"
-    mv ~/.zshrc ~/zshrc.bkp
-fi
+# --------------------
 
-ln -s .dotfiles/zsh/zshrc $HOME/.zshrc
-if [ $? -ne 0 ];then
-    echo "Link Failed"
-fi 
+setup_zsh()
+{
+    cd $HOME
+    if [ -f .zshrc ]; then
+        echo ".zshrc exists. Taking backup"
+        mv .zshrc zshrc.bkp
+    fi
 
-mkdir -p .antigen
-curl -L git.io/antigen > .antigen/antigen.zsh
-source .antigen/antigen.zsh
-if [ $? -ne 0 ];then
-    echo "source Failed"
-fi
-antigen apply
-if [ $? -ne 0 ];then
-    echo "apply Failed"
-fi
+    ln -s .dotfiles/zsh/zshrc .zshrc
 
-#chsh -s /bin/zsh
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+    chsh -s /bin/zsh
+}
 
-# 2. Install VIM setup
+# 2. Install NeoVIM setup
+# -----------------------
+
+setup_vim()
+{
+    mkdir -p .config/nvim/
+    cd .config/nvim
+
+    if [ -f init.vim ]; then
+        echo "init.vim exists. Taking backup"
+        mv init.vim init.vim.bkp
+    fi
+
+    ln -s ~/.dotfiles/vim/init.vim .
+
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    #Install all plugins
+    nvim +PlugInstall +UpdateRemotePlugins +qa
+}
 
 # 3. Install TMUX setup
+# ---------------------
+
+setup_tmux()
+{
+    cd $HOME
+    if [ -f .tmux.conf ]; then
+        echo ".tmux.conf exists. Taking backup"
+        mv .tmux.conf tmux.conf.bkp
+    fi
+    ln -s .dotfiles/tmux/tmux.conf .tmux.conf
+
+    mkdir -p .tmux/plugins/
+    cd .tmux/plugins
+
+    git clone --depth 1 https://github.com/tmux-plugins/tpm.git
+}
+
+setup_zsh 
+setup_vim
+setup_tmux
