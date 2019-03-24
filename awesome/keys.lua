885468af1b3641ac038c7ebdbd6a152ffdd3dcc7
AadityaNair <aadityanair6494@gmail.com>
2019-03-24 23:50:14 +0530

    Added application switcher and move to urgent

--- awesome/keys.lua
+++ awesome/keys.lua
@@ -60,7 +60,14 @@ keys.globalkeys = gears.table.join(
 ---- Clients
     awful.key({modkey}, "Right", function () awful.client.focus.byidx( 1) end, {description = "focus next by index", group = "client"}),
     awful.key({modkey}, "Left", function () awful.client.focus.byidx(-1) end, {description = "focus previous by index", group = "client"}),
-
+    awful.key({modkey}, "u", awful.client.urgent.jumpto, {description = "jump to urgent client", group = "client"}),
+    awful.key({modkey}, "Tab",
+    function ()
+        awful.client.focus.history.previous()
+        if client.focus then
+            client.focus:raise()
+        end
+    end, {description = "go back", group = "client"}),
 
 ---- Layouts
     awful.key({modkey, shiftkey}, "Right", function() awful.client.swap.byidx(  1) end, {description = "swap with next client by index", group = "client"}),
@@ -82,22 +89,6 @@ keys.globalkeys = gears.table.join(
     awful.key({modkey, shiftkey}, "q", awesome.quit, {description = "quit awesome", group = "awesome"})
 )
 --------------------------------------------
-
-
-    --awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
-              --{description = "jump to urgent client", group = "client"}),
-    --awful.key({ modkey,           }, "Tab",
-        --function ()
-            --awful.client.focus.history.previous()
-            --if client.focus then
-                --client.focus:raise()
-            --end
-        --end,
-        --{description = "go back", group = "client"}),
-
-    -- Standard program
-
-     --awful.key({ modkey, "Shift"}, "space", function() awful.layout.inc(-1) end, {description = "select previous", group = "layout"}),
 
     --awful.key({ modkey }, "x",
               --function ()
