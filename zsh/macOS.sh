eb241a9861d0447f4a9b1c36618531a483797251
AadityaNair <AadityaNair@users.noreply.github.com>
2023-07-17 22:40:49 +0100

    [zsh] Move homebrew configuration to macos specific file

--- zsh/macOS.sh
+++ zsh/macOS.sh
@@ -1,3 +1,11 @@
 alias caps="screencapture -c" # Capture entire screen
 alias capa="screencapture -i -c" # Capture a specified area
 alias capw="screencapture -i -w -c" # Capture a window
+
+
+export HOMEBREW_PREFIX="/opt/homebrew";
+export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
+export HOMEBREW_REPOSITORY="/opt/homebrew";
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
+export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
