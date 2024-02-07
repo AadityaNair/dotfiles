79f7e5614e4fa35e94014c6018e52a3fe9bf7ea2
Aaditya M Nair <aadityanair6494@gmail.com>
2024-02-07 06:05:35 -0800

    [vim] Add leftover configuration from nvim-cmp

--- vim/init.lua
+++ vim/init.lua
@@ -207,6 +207,12 @@ cmp.setup.cmdline(':', {
     })
 })
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+    capabilities = capabilities
+  }
+
 
 --------------------- Other configuration
 
