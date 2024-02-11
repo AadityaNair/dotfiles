1fbbcc1c1ed5cf3872bf39fa4f6b7dd10a66c977
AadityaNair <AadityaNair@users.noreply.github.com>
2024-02-11 20:35:06 +0000

    [zsh] pnpm variables

--- zsh/macOS.sh
+++ zsh/macOS.sh
@@ -12,3 +12,11 @@ export HOMEBREW_REPOSITORY="/opt/homebrew";
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+
+# pnpm
+export PNPM_HOME="/Users/aaditya/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
