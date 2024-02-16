317066496d2401f5e2ab0a6b5cc348c9a01a8bc5
AadityaNair <AadityaNair@users.noreply.github.com>
2024-02-16 15:47:05 +0000

    [vim] Move default keybindings to top.

--- vim/init.lua
+++ vim/init.lua
@@ -2,17 +2,73 @@
 --  mbbill/undotree                 -> debugloop/telescope-undo.nvim
 --  terryma/vim-multiple-cursors    -> mg979/vim-visual-multi (?X)
 
+
+----------------------------------------------------  Other configuration   ------------------------------------------
+
 vim.g.python3_host_prog="/usr/bin/python3"
 vim.g.loaded_node_provider=0 
 vim.g.loaded_perl_provider=0
 vim.g.loaded_python_provider=0
+
 vim.g.mapleader = ","
+vim.opt.number = true
+vim.opt.textwidth = 0
+vim.opt.wrapmargin = 0
+vim.opt.ruler = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+-- vim.opt.showmode = true
+-- vim.opt.hidden = true
+
+vim.keymap.set('i', 'kj', '<Esc>')
+vim.keymap.set('n', ';', ':')
+vim.keymap.set('n', '<leader><space>', ':nohlsearch<CR>', {silent = true})
+
+vim.keymap.set('n', '<Leader>r', '<Cmd>edit!<CR>')
+
+vim.keymap.set('n','<C-j>',':m .+1<CR>==',{silent = true})
+vim.keymap.set('n','<C-k>',':m .-2<CR>==',{silent = true})
+vim.keymap.set('i','<C-j>','<Esc>:m .+1<CR>==gi',{silent = true})
+vim.keymap.set('i','<C-k>','<Esc>:m .-2<CR>==gi',{silent = true})
+vim.keymap.set('v','<C-j>',":m '>+1<CR>gv=gv",{silent = true})
+vim.keymap.set('v','<C-k>',":m '<-2<CR>gv=gv",{silent = true})
+
+vim.cmd('cnoreabbrev W! w!')
+vim.cmd('cnoreabbrev Q! q!')
+vim.cmd('cnoreabbrev Qall! qall!')
+vim.cmd('cnoreabbrev Wq wq')
+vim.cmd('cnoreabbrev Wa wa')
+vim.cmd('cnoreabbrev wQ wq')
+vim.cmd('cnoreabbrev WQ wq')
+vim.cmd('cnoreabbrev W w')
+vim.cmd('cnoreabbrev Q q')
+vim.cmd('cnoreabbrev Qall qall')
+
+-- Copied from https://github.com/creativenull/dotfiles/blob/9ae60de4f926436d5682406a5b801a3768bbc765/config/nvim/init.lua#L70-L86
+-- Return to the previous cursor position.
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
 
 ------------------------------------------------- Plugins Setup --------------------------------------------------
 
 -- TODO: vista.vim, trouble.nvim
 -- TODO: Full Refactor: Looks, autocomplete, Work file, 
--- TODO: Bring the default keymapings to the top so that those work even if some plugins break things.
 -- TODO: When there is an error, the error window goes away too fast to actually read the thing.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
@@ -245,66 +301,10 @@ require('lspconfig')['bashls'].setup {
     capabilities = capabilities
   }
 
-
-----------------------------------------------------  Other configuration   ------------------------------------------
-
-vim.opt.number = true
-vim.opt.textwidth = 0
-vim.opt.wrapmargin = 0
-vim.opt.ruler = true
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
--- vim.opt.showmode = true
--- vim.opt.hidden = true
-
-vim.keymap.set('i', 'kj', '<Esc>')
-vim.keymap.set('n', ';', ':')
-vim.keymap.set('n', '<leader><space>', ':nohlsearch<CR>', {silent = true})
-
-vim.keymap.set('n', '<Leader>r', '<Cmd>edit!<CR>')
-
-vim.keymap.set('n','<C-j>',':m .+1<CR>==',{silent = true})
-vim.keymap.set('n','<C-k>',':m .-2<CR>==',{silent = true})
-vim.keymap.set('i','<C-j>','<Esc>:m .+1<CR>==gi',{silent = true})
-vim.keymap.set('i','<C-k>','<Esc>:m .-2<CR>==gi',{silent = true})
-vim.keymap.set('v','<C-j>',":m '>+1<CR>gv=gv",{silent = true})
-vim.keymap.set('v','<C-k>',":m '<-2<CR>gv=gv",{silent = true})
-
-vim.cmd('cnoreabbrev W! w!')
-vim.cmd('cnoreabbrev Q! q!')
-vim.cmd('cnoreabbrev Qall! qall!')
-vim.cmd('cnoreabbrev Wq wq')
-vim.cmd('cnoreabbrev Wa wa')
-vim.cmd('cnoreabbrev wQ wq')
-vim.cmd('cnoreabbrev WQ wq')
-vim.cmd('cnoreabbrev W w')
-vim.cmd('cnoreabbrev Q q')
-vim.cmd('cnoreabbrev Qall qall')
-
--- Copied from https://github.com/creativenull/dotfiles/blob/9ae60de4f926436d5682406a5b801a3768bbc765/config/nvim/init.lua#L70-L86
--- Return to the previous cursor position.
-vim.api.nvim_create_autocmd('BufReadPost', {
-  callback = function(args)
-    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
-    local not_commit = vim.b[args.buf].filetype ~= 'commit'
-
-    if valid_line and not_commit then
-      vim.cmd([[normal! g`"]])
-    end
-  end,
-})
-
 -- Set the right filetypes.
 vim.filetype.add({
  filename = {
+    --TODO: Comment-nvim doesn't work with starlark.
     ['TARGETS'] = 'starlark',
     --TODO: Make *.zsh look like bash. Below doesn't do it.
     ['*.zsh'] = 'bash',
