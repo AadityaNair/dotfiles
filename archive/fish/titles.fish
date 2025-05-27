215d0f557287728f1dc3ba948d42dccb6940466a
Aaditya M Nair <aadityanair6494@gmail.com>
2025-05-27 13:35:13 -0700

    [fish] Added a few more titles

--- archive/fish/titles.fish
+++ archive/fish/titles.fish
@@ -6,20 +6,43 @@
 # Before the command run $argv[1] is the command itself
 # After the command run $argv[1] is `fish`
 
-function rename
+function terminal_rename
 end
 
 if test -n "$ZELLIJ"
-    function rename
+    function terminal_rename
         zellij action rename-tab "$argv[1]"
     end
 else if test -n "$TMUX"
-    function rename
+    function terminal_rename
         printf "\033k%s\033\\" "$argv[1]"
     end
 end
 
 # TODO: Fully build this function
 function fish_title
+    set -l cmd (string split ' ' "$argv[1]")
+    set -l cnt (count $cmd)
+
+    switch $argv[1]
+        case man run-help
+            if test $cnt -eq 3
+                terminal_rename "$cmd[3]\($cmd[2]\) ❓"
+            else
+                terminal_rename "$argv[2] ❓"
+            end
+
+        case ssh
+            terminal_rename "$cmd[2]"
+
+        case sudo
+            terminal_rename "⚡$cmd[2]"
+
+        case fish  # After the command has run.
+            terminal_rename "(pwd)"
+
+        case *
+            terminal_rename "$cmd[1]"
+    end
 end
 
