3384c166b94f041e455e85c5349c44506ee3a0df
Aaditya M Nair <aadityanair6494@gmail.com>
2025-05-27 05:02:58 -0700

    [fish] Very initial port of zsh configs to fish

--- archive/fish/config.fish
+++ archive/fish/config.fish
@@ -1,4 +1,28 @@
 if status is-interactive
+    ### SETUP VARIABLES
+    set -x INSTALL "$HOME/.config"
+    set -x XDG_CACHE_HOME "$HOME/.local/cache"
+    set -x TERM "tmux-256color"
+    set -x MANPAGER "bat -l man -p" # TODO: Find a better manpager
+    set -x EDITOR "nvim"
+
+
+    ### COSMETIC SETTINGS
+    set -x STARSHIP_CONFIG "$INSTALL/dotfiles/zsh/starship.toml"
+    starship init fish | source
+    # TODO: Configure titles
+
+    ### HISTORY SETTINGS
+    # TODO: Export atuin config file location here.
+    # TODO: Add function for the up arrow function.
     atuin init fish | source
+
+    ### AUTOCOMPLETE SETTINGS
+
+    ### COMMON SHELL ALIASES
+
+    ### OTHERS
     zoxide init fish | source
+    # TODO: Configure zoxide
 end
+ # vim: ft=fish
