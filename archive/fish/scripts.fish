21e9003e33e20b94cfaf0ce8380eddf67060cdb0
Aaditya M Nair <aadityanair6494@gmail.com>
2025-05-29 08:36:37 -0700

    [fish] Extract now works on tar.xz files. Also add quotes to cases

--- archive/fish/scripts.fish
+++ archive/fish/scripts.fish
@@ -78,27 +78,29 @@ function extract
     end
 
     switch $argv[1]
-        case *.tar.bz2 *.tbz2
+        case "*.tar.bz2" "*.tbz2"
             tar xjf "$argv[1]"
-        case *.tar.gz *.tgz
+        case "*.tar.gz" "*.tgz"
             tar xzf "$argv[1]"
-        case *.bz2
+        case "*.tar.xz"
+            tar xf "$argv[1]"
+        case "*.bz2"
             bunzip2 $argv[1]
-        case *.rar
+        case "*.rar"
             unrar x $argv[1]
-        case *.gz
+        case "*.gz"
             gunzip $argv[1]
-        case *.tar
+        case "*.tar"
             tar xf $argv[1]
-        case *.zip
+        case "*.zip"
             unzip $argv[1]
-        case *.Z
+        case "*.Z"
             uncompress $argv[1]
-        case *.7z
+        case "*.7z"
             7zr e $argv[1]
-        case *.rpm
+        case "*.rpm"
             rpm2cpio $argv[1] | cpio -idmv
-        case *
+        case "*"
             echo "Can't `extract` this file. Please check."
     end
 end
