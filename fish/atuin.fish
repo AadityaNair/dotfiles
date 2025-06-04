dec987a2b276fd5edcc6a470279a0d567d18c3bf
AadityaNair <AadityaNair@users.noreply.github.com>
2025-06-04 22:24:54 +0100

    [fish][atuin] We don't need the condition of enter accept once we have disabled that feature

--- fish/atuin.fish
+++ fish/atuin.fish
@@ -18,7 +18,6 @@ function _atuin_postexec --on-event fish_postexec
     set -l s $status # This is done so that we have the command status before next commands override it.
 
     if test -n "$ATUIN_HISTORY_ID"
-        # todo: Maybe we can speed this up by supplying $CMD_DURATION to the script directly?
         ATUIN_LOG=error atuin history end --exit $s -- $ATUIN_HISTORY_ID &>/dev/null &
         disown
     end
@@ -32,14 +31,7 @@ function _atuin_search
     set -l ATUIN_H "$(ATUIN_SHELL_FISH=t ATUIN_LOG=error ATUIN_QUERY=(commandline -b) atuin search --keymap-mode=$keymap_mode $argv -i 3>&1 1>&2 2>&3)"
 
     if test -n "$ATUIN_H"
-        if string match --quiet '__atuin_accept__:*' "$ATUIN_H"
-          set -l ATUIN_HIST "$(string replace "__atuin_accept__:" "" -- "$ATUIN_H")"
-          commandline -r "$ATUIN_HIST"
-          commandline -f repaint
-          return
-        else
-          commandline -r "$ATUIN_H"
-        end
+        commandline -r "$ATUIN_H"
     end
 
     commandline -f repaint
