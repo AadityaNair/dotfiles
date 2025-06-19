a4fe518487d67cb4c2769b60a0287c205baec2ba
Aaditya M Nair <aadityanair6494@gmail.com>
2025-06-19 03:38:28 -0700

    [fish][starship] Simplify code

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -8,11 +8,14 @@ set -x STARSHIP_CONFIG "$DOTFILES/shell_applications/starship.toml"
 function fish_prompt
     set STARSHIP_CMD_PIPESTATUS $pipestatus
     set STARSHIP_CMD_STATUS $status
-    # Account for changes in variable name between v2.7 and v3.0
-    set STARSHIP_DURATION "$CMD_DURATION$cmd_duration"
+    set STARSHIP_DURATION "$CMD_DURATION"
     set STARSHIP_JOBS (count (jobs -p))
-    #TODO: Consider not passing things we don't care about.
-    starship prompt --terminal-width="$COLUMNS" --status=$STARSHIP_CMD_STATUS --pipestatus="$STARSHIP_CMD_PIPESTATUS" --cmd-duration=$STARSHIP_DURATION --jobs=$STARSHIP_JOBS
+    # TODO: Consider not passing things we don't care about.
+    starship prompt --terminal-width="$COLUMNS"\
+                    --status=$STARSHIP_CMD_STATUS\
+                    --pipestatus="$STARSHIP_CMD_PIPESTATUS"\
+                    --cmd-duration=$STARSHIP_DURATION\
+                    --jobs=$STARSHIP_JOBS
 end
 
 # Disable virtualenv prompt, it breaks starship
@@ -24,6 +27,5 @@ builtin functions -e fish_mode_prompt
 set -gx STARSHIP_SHELL "fish"
 
 # Set up the session key that will be used to store logs
-# We don't use `random [min] [max]` because it is unavailable in older versions of fish shell
 # TODO: Consider if we still need this
-set -gx STARSHIP_SESSION_KEY (string sub -s1 -l16 (random)(random)(random)(random)(random)0000000000000000)
+set -gx STARSHIP_SESSION_KEY (random 1000000000000000 9999999999999999)
