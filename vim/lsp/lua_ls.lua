c0c5f8c1fe4d010059393db3eda2c8120ff1d55f
AadityaNair <AadityaNair@users.noreply.github.com>
2025-09-20 21:07:44 +0100

    [vim] Add the default configuration for lua_ls from lsp_config

---
+++ vim/lsp/lua_ls.lua
@@ -0,0 +1,21 @@
+-- We only use lua_ls for neovim config (for now, atleast)
+-- So this config is built based on that.
+-- Graciously copied from:
+--   https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
+--   https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
+
+---@type vim.lsp.Config
+return {
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = {
+        '.luarc.json',
+        '.luarc.jsonc',
+        '.luacheckrc',
+        '.stylua.toml',
+        'stylua.toml',
+        'selene.toml',
+        'selene.yml',
+        '.git',
+    },
+}
