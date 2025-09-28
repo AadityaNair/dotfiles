f4b069cf8db3df5ab0ab7c370a362e0e243396d6
AadityaNair <AadityaNair@users.noreply.github.com>
2025-09-28 11:35:23 +0100

    [fish] Remove possibly unnecessary things

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -11,6 +11,7 @@ function fish_prompt
     set STARSHIP_CMD_STATUS $status
     set STARSHIP_DURATION "$CMD_DURATION"
     set STARSHIP_JOBS (count (jobs -p))
+
     # TODO: Consider not passing things we don't care about.
     starship prompt --terminal-width="$COLUMNS"\
                     --status=$STARSHIP_CMD_STATUS\
@@ -19,14 +20,3 @@ function fish_prompt
                     --jobs=$STARSHIP_JOBS
 end
 
-# Disable virtualenv prompt, it breaks starship
-set -g VIRTUAL_ENV_DISABLE_PROMPT 1
-
-# Remove default mode prompt
-builtin functions -e fish_mode_prompt
-
-set -gx STARSHIP_SHELL "fish"
-
-# Set up the session key that will be used to store logs
-# TODO: Consider if we still need this
-set -gx STARSHIP_SESSION_KEY (random 1000000000000000 9999999999999999)
