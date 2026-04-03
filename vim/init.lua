820e41a4b2ee2a4d80c2d0d0f94b131e00c840ce
AadityaNair <AadityaNair@users.noreply.github.com>
2026-04-03 11:22:37 +0100

    [vim] Move the 00default config to init.lua

--- vim/init.lua
+++ vim/init.lua
@@ -1,33 +1,70 @@
--- TODO: Move to nvim 0.12: plugin-manager
 -- TODO: Evaluate all the current plugins
--- TODO: Move to autoloading configs as described in https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack.html#many-vim-pack-add
 -- TODO: Profile startup time
 
-local common = require("common")
-local TableConcat = common.TableConcat
+-- Bare minimum builtin stuff to make experience tolerable.
+-- All the fancy shit in the plugin/
+-- This file should work even if everything else breaks.
 
-------------------------------------------------- Plugins Setup --------------------------------------------------
+vim.g.python3_host_prog = "/usr/bin/python3"
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider = 0
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = ","
+vim.opt.number = true
+vim.opt.textwidth = 0
+vim.opt.wrapmargin = 0
+vim.opt.ruler = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
--- local ui = require("ui")
--- local ux = require("ux")
--- local coding = require("coding")
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+-- vim.opt.showmode = true
+-- vim.opt.hidden = true
+-- add function to get length of line
+vim.opt.clipboard = "unnamedplus" -- Integrate vim with system clipboard.
 
--- local plugins = {}
+vim.keymap.set("i", "kj", "<Esc>")
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("n", "<leader><space>", ":nohlsearch<CR>", { silent = true })
 
--- plugins = TableConcat(plugins, ui.plugins)
--- plugins = TableConcat(plugins, ux.plugins)
--- plugins = TableConcat(plugins, coding.plugins)
+vim.keymap.set("n", "<Leader>r", "<Cmd>edit!<CR>")
 
--- require("lazy").setup({
---     spec = plugins,
---     rocks = {
---         enabled = false,
---     },
--- })
+vim.keymap.set("n", "<C-j>", ":m .+1<CR>==", { silent = true })
+vim.keymap.set("n", "<C-k>", ":m .-2<CR>==", { silent = true })
+vim.keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi", { silent = true })
+vim.keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi", { silent = true })
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true })
 
--- ui.setup()
--- ux.setup()
--- coding.setup()
+vim.cmd("cnoreabbrev W! w!")
+vim.cmd("cnoreabbrev Q! q!")
+vim.cmd("cnoreabbrev Qall! qall!")
+vim.cmd("cnoreabbrev Wq wq")
+vim.cmd("cnoreabbrev Wa wa")
+vim.cmd("cnoreabbrev wQ wq")
+vim.cmd("cnoreabbrev WQ wq")
+vim.cmd("cnoreabbrev W w")
+vim.cmd("cnoreabbrev Q q")
+vim.cmd("cnoreabbrev Qall qall")
+
+-- Copied from https://github.com/creativenull/dotfiles/blob/main/config/nvim/init.lua#L70-L80
+-- Return to the previous cursor position.
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(args)
+        -- TODO: When line is invalid, do something more intelligent, or go to the end.
+        local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line("$")
+        local not_commit = vim.b[args.buf].filetype ~= "commit"
+
+        if valid_line and not_commit then
+            vim.cmd([[normal! g`"]])
+        end
+    end,
+})
