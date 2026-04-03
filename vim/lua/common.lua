53b5b7c542d40a5a2712166b8f77dbc06f10aeff
AadityaNair <AadityaNair@users.noreply.github.com>
2026-04-03 22:48:07 +0100

    [vim] Remove tableconcat function

--- vim/lua/common.lua
+++ vim/lua/common.lua
@@ -1,12 +1,5 @@
 local module = {}
 
-function module.TableConcat(t1, t2)
-    for _, v in ipairs(t2) do
-        require("table").insert(t1, v)
-    end
-    return t1
-end
-
 function module.gh_url(repo)
     return string.format("https://github.com/%s.git", repo)
 end
