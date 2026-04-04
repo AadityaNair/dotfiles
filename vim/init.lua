0d8182227921bd2da62d26442f621b1a3b7ca7b4
Aaditya M Nair <aadityanair6494@gmail.com>
2026-04-04 06:03:28 -0700

    [vim] Move to EOF when lines are deleted

--- vim/init.lua
+++ vim/init.lua
@@ -66,12 +66,15 @@ vim.cmd("cnoreabbrev Qall qall")
 -- Return to the previous cursor position.
 vim.api.nvim_create_autocmd("BufReadPost", {
     callback = function(args)
-        -- TODO: When line is invalid, do something more intelligent, or go to the end.
-        local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line("$")
-        local not_commit = vim.b[args.buf].filetype ~= "commit"
+        if vim.b[args.buf].filetype == "commit" then return end
 
-        if valid_line and not_commit then
+        local mark_line = vim.fn.line([['"]])
+        local last_line = vim.fn.line("$")
+
+        if mark_line >= 1 and mark_line <= last_line then
             vim.cmd([[normal! g`"]])
+        else
+            vim.cmd("normal! G")
         end
     end,
 })
