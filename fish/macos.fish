e96d6397b84c84e77c11ae6393653009aaa78c9a
AadityaNair <AadityaNair@users.noreply.github.com>
2026-04-07 11:58:47 +0100

    [fish] Add some env variables so that applications to pollute my homedir

--- fish/macos.fish
+++ fish/macos.fish
@@ -10,10 +10,15 @@ abbr -a pp pbpaste
 set -x HOMEBREW_PREFIX /opt/homebrew
 set -x HOMEBREW_CELLAR /opt/homebrew/Cellar
 set -x HOMEBREW_REPOSITORY /opt/homebrew
-set -x PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
 set -x MANPATH /opt/homebrew/share/man $MANPATH
 set -x INFOPATH /opt/homebrew/share/info $INFOPATH
 
 set -x RUSTUP_HOME ~/.config/rustup
 set -x CARGO_HOME ~/.local/share/cargo
 set -a PATH ~/.config/rustup/toolchains/stable-aarch64-apple-darwin/bin/
+
+
+# Bunch of applications which create folders in homedir. Currently only for personal laptop
+set -x GEMINI_CONFIG_DIR "/Users/aaditya/.config/gemini"
+set -x CLAUDE_CONFIG_DIR "/Users/aaditya/.config/claude"
+set -x COPILOT_HOME "/Users/aaditya/.config/copilot"
