6b4da80da71108827c48253cd7a3551fdbe1283a
AadityaNair <AadityaNair@users.noreply.github.com>
2026-04-11 23:54:20 +0100

    [vim] Add a TODO and lint

--- vim/plugin/completion.lua
+++ vim/plugin/completion.lua
@@ -4,50 +4,51 @@ vim.pack.add({
     { src = gh_url("saghen/blink.cmp"), version = vim.version.range("*") },
 })
 
-require("blink.cmp").setup(
-    {
-        keymap = {
-            preset = "none",
-            ["<Up>"] = { "select_prev", "fallback" },
-            ["<Down>"] = { "select_next", "fallback" },
-            ["<Tab>"] = { "show", "accept", "insert_next", "fallback" },
-            ["<S-Tab>"] = { "insert_prev", "fallback" },
-            ["<Esc>"] = { "hide", "fallback" },
-        },
-        appearance = {
-            nerd_font_variant = "mono",
-        },
-        completion = {
-            documentation = { auto_show = false },
-            ghost_text = { enabled = true, show_with_menu = false },
-            list = { selection = { preselect = true, auto_insert = true } },
-            keyword = { range = "full" },
-            menu = {
-                draw = {
-                    columns = {
-                        { "kind_icon" },
-                        { "label", "label_description", gap = 1 },
-                        { "source_name" },
-                    },
-                    components = {
-                        label = {
-                            text = function(ctx)
-                                return require("colorful-menu").blink_components_text(ctx)
-                            end,
-                            highlight = function(ctx)
-                                return require("colorful-menu").blink_components_highlight(ctx)
-                            end,
-                        },
-                    },
+-- TODO: Setting this to true conflicts with blink.cmp. Research more.
+vim.o.autocomplete = false
 
-                    treesitter = { "lsp" },
+require("blink.cmp").setup({
+    keymap = {
+        preset = "none",
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<Tab>"] = { "show", "accept", "insert_next", "fallback" },
+        ["<S-Tab>"] = { "insert_prev", "fallback" },
+        ["<Esc>"] = { "hide", "fallback" },
+    },
+    appearance = {
+        nerd_font_variant = "mono",
+    },
+    completion = {
+        documentation = { auto_show = false },
+        ghost_text = { enabled = true, show_with_menu = false },
+        list = { selection = { preselect = true, auto_insert = true } },
+        keyword = { range = "full" },
+        menu = {
+            draw = {
+                columns = {
+                    { "kind_icon" },
+                    { "label", "label_description", gap = 1 },
+                    { "source_name" },
                 },
+                components = {
+                    label = {
+                        text = function(ctx)
+                            return require("colorful-menu").blink_components_text(ctx)
+                        end,
+                        highlight = function(ctx)
+                            return require("colorful-menu").blink_components_highlight(ctx)
+                        end,
+                    },
+                },
+
+                treesitter = { "lsp" },
             },
         },
-        sources = {
-            default = { "lsp", "path", "snippets", "buffer" },
-        },
-        fuzzy = { implementation = "prefer_rust" },
-        signature = { enabled = true },
-    }
-)
+    },
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+    },
+    fuzzy = { implementation = "prefer_rust" },
+    signature = { enabled = true },
+})
