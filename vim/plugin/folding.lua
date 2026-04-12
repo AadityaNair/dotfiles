da4784eac68aeef2f5187e0b12fe2c1c0f234853
AadityaNair <AadityaNair@users.noreply.github.com>
2026-04-12 13:04:42 +0100

    [vim] Folding doesn't expand with the depth of folds

--- vim/plugin/folding.lua
+++ vim/plugin/folding.lua
@@ -1,6 +1,8 @@
-vim.o.foldcolumn = "auto:9"
+vim.o.foldcolumn = "auto:1"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.foldtext = ""
-vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:]]
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:]]
+
+vim.keymap.set("n", "f", "za")
